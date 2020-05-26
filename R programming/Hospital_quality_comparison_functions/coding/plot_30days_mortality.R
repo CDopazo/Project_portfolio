326299006fa92ce18a5a68e0bd4fc237f0d4c0ea
Carlos Dopazo <33837781+CDopazo@users.noreply.github.com>
2020-05-26 17:55:43 +0200

    Delete plot_30days_mortality.R

--- R programming/Hospital_quality_comparison_functions/coding/plot_30days_mortality.R
+++
@@ -1,8 +0,0 @@
-outcome <- read.csv("data/outcome-of-care-measures.csv", colClasses = "character")
-outcome[, 11] <- as.numeric(outcome[, 11])
-hist(outcome[, 11],
-     main=NULL,
-     xlab="30-day death rates from heart attack",
-     ylab= "Hospitals Frequency",
-     border="black",
-     col="red")
